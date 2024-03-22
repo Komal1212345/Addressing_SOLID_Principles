a552a7ed7f7f7dcfa89974485685678e52ecdd1d
Komal1212345 <129340112+Komal1212345@users.noreply.github.com>
2024-03-22 16:59:33 -0600

    Create Square.java

---
+++ solved/LSP/Square.java
@@ -0,0 +1,19 @@
+public class Square {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double calculateArea() {
+        return side * side;
+    }
+}
