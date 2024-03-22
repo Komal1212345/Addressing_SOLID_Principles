8990aa7734945859b70bf32e29cf0951521b0b72
Komal1212345 <129340112+Komal1212345@users.noreply.github.com>
2024-03-22 16:52:03 -0600

    Update DiscountCalculator.java

--- violated/OCP/DiscountCalculator.java
+++ violated/OCP/DiscountCalculator.java
@@ -1,1 +1,11 @@
-
+public class DiscountCalculator {
+    public double calculateDiscount(Product product) {
+        if (product.getType().equals("Book")) {
+            return product.getPrice() * 0.1; // 10% discount for books
+        } else if (product.getType().equals("Electronics")) {
+            return product.getPrice() * 0.05; // 5% discount for electronics
+        } else {
+            return 0; // No discount for other product types
+        }
+    }
+}
